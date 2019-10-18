903672f5bd04cadf99178e4fe337722331f81b14
mickymck <mickymckeon@gmail.com>
2019-10-17 23:23:57 -0400

    got my elevations, max, and min working, but my points are broken

--- pathfinder.py
+++ pathfinder.py
@@ -7,12 +7,14 @@ bg = Image.new('RGBA', (600, 600), (0, 255, 0, 255))
 
 class Map():
     def __init__(self, raw_file):
-        self.data = create_data_from_raw_data(raw_file)
-        self.point_collection = self.create_points(self.data)
+        self.elevation_list = self.create_data_from_raw_data(raw_file)
+        self.max_elevation = self.find_max()
+        self.min_elevation = self.find_min()
+        self.point_collection = self.create_points()
 
 # imports the txt file, uses readlines to create one array while keeping the lines separate, looping through each string in that list and splitting each list item in the string, so that is is an array of strings, ultimately resulting in an array of 600 arrays or 600 strings, so that each array is a new row, and each string is at a unique index—column—within its row
 
-    def create_data_from_raw_data(raw_file):
+    def create_data_from_raw_data(self, raw_file):
         with open (raw_file) as file:
             elevation_lines = file.readlines()
             elevation_list = []
@@ -20,12 +22,29 @@ class Map():
                 elevation_list.append(line.split())
             return elevation_list
 
+    def find_max(self):
+        max_elevation = int(self.elevation_list[0][0])
+        for elevation_row in self.elevation_list:
+            for elevation in elevation_row:
+                if int(elevation) > max_elevation:
+                    max_elevation = int(elevation)
+        return max_elevation
+
+    def find_min(self):
+        min_elevation = int(self.elevation_list[0][0])
+        for elevation_row in self.elevation_list:
+            for elevation in elevation_row:
+                if int(elevation) < min_elevation:
+                    min_elevation = int(elevation)
+        return min_elevation
+
 
 # a method to create my points and store them in a list
-    def create_points():
+
+    def create_points(self):
         points = []
-        for datapoint in self.data:
-            coord = (self.data[:][x], self.data[y])
+        for datapoint in self.elevation_list[elevation_row]:
+            coord = (self.elevation_list[elevation_row][i], self.elevation_list[i])
             elevation = int(datapoint)
             map = self
             point = Point(coord, elevation, map)
@@ -42,7 +61,7 @@ class Point():
         self.alpha = elevation_to_alpha()
         self.map = map
 
-    def elevation_to_alpha(elevation):
+    def elevation_to_alpha(self, elevation):
         max_elevation = self.map.max_elevation
         min_elevation = self.map.min_elevation
         elevation_range = max_elevation - min_elevation
@@ -51,19 +70,6 @@ class Point():
 
 Map("elevation_small.txt")
 
-
-# def find_max_min(elevation_list_file):
-
-#     max_elevation = int(elevation_list[0])
-#     min_elevation = int(elevation_list[0])
-#     for elevation in elevation_list:
-#         if int(elevation) > max_elevation:
-#             max_elevation = int(elevation)
-#         if int(elevation) < min_elevation:
-#             min_elevation = int(elevation)
-#     print(elevation_list)
-#     print(max_elevation)
-#     print(min_elevation)
 
 # find_max_min("elevation_small.txt")
 
