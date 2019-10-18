851a0464c5e353c50ab92d5c978666bd307052c0
mickymck <mickymckeon@gmail.com>
2019-10-17 22:35:13 -0400

    trouble with my create_data_from_raw_data function

--- pathfinder.py
+++ pathfinder.py
@@ -4,62 +4,52 @@ from PIL import Image
 bg = Image.new('RGBA', (600, 600), (0, 255, 0, 255))
 
 # bg.show()
-    
-def create_data_from_raw_data(elevation_list_file):
-    with open (elevation_list_file) as file:
-        elevation_lines = file.readlines()
-        elevation_list = []
-        for line in elevation_lines:
-            elevation_list.append(line.split())
-        print(elevation_list)
 
-create_data_from_raw_data("elevation_small.txt")
+class Map():
+    def __init__(self, raw_file):
+        self.data = create_data_from_raw_data(raw_file)
+        self.point_collection = self.create_points(self.data)
 
-# class Map():
-#     def __init__(self, raw_file):
-#         self.data = create_data_from_raw_data()
-#         self.point_collection = self.create_points()
+# imports the txt file, uses readlines to create one array while keeping the lines separate, looping through each string in that list and splitting each list item in the string, so that is is an array of strings, ultimately resulting in an array of 600 arrays or 600 strings, so that each array is a new row, and each string is at a unique index—column—within its row
 
-
-    
-# # a method to transform the raw data into a list of lists
-#     def create_data_from_raw_data(elevation_list_file):
-#         with open (elevation_list_file) as file:
-#             elevation_list = file.read().splitlines()
-    
-#     create_data_from_raw_data("elevation_small.txt")
+    def create_data_from_raw_data(raw_file):
+        with open (raw_file) as file:
+            elevation_lines = file.readlines()
+            elevation_list = []
+            for line in elevation_lines:
+                elevation_list.append(line.split())
+            return elevation_list
 
 
-# # a method to create my points and store them in a list
-#     def create_points():
-#         points = []
-#         for datapoint in self.data:
-#             # TODO - remove hard coded data after you get back into it
-#             coord = (2,0)
-#             elevation = 100
-#             map = self
-#             point = Point(coord, elevation, map)
-#             points.append(point)
-#         return points
+# a method to create my points and store them in a list
+    def create_points():
+        points = []
+        for datapoint in self.data:
+            coord = (self.data[:][x], self.data[y])
+            elevation = int(datapoint)
+            map = self
+            point = Point(coord, elevation, map)
+            points.append(point)
+        print(points)
 
 
 
     
 
-# class Point():
-#     def __init__(self, coords, elevation, map)
-#         self.coords = coords
-#         self.alpha = elevation_to_alpha()
-#         self.map = map
+class Point():
+    def __init__(self, coords, elevation, map):
+        self.coords = coords
+        self.alpha = elevation_to_alpha()
+        self.map = map
 
-#     def elevation_to_alpha(elevation):
-#         max_elevation = self.map.max_elevation
-#         min_elevation = self.map.min_elevation
-#         elevation_range = max_elevation - min_elevation
-#         return (elevation/elevation_range) * 255
+    def elevation_to_alpha(elevation):
+        max_elevation = self.map.max_elevation
+        min_elevation = self.map.min_elevation
+        elevation_range = max_elevation - min_elevation
+        return (elevation/elevation_range) * 255
 
 
-
+Map("elevation_small.txt")
 
 
 # def find_max_min(elevation_list_file):
